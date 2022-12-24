17fe31e5883808b4f9b2e9b6430b7056b7b70536
layerunk <89783661+layerunk@users.noreply.github.com>
2022-12-24 11:39:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,10 +21,10 @@ function clickity(){
 }
 
 //PARAMETER 2 IS IN speak
-function speak(){
+function speak12(){
     console.log("You have passed Parameter2")
-    spetext = "The first prediction is " + hatext1 + "and ";
-    spetext = "The second prediction is " + hatext2;
+    spetext1 = "The first prediction is " + hatext1 + "and ";
+    spetext2 = "The second prediction is " + hatext2;
 
     synth = window.speechSynthesis;
     utterThis = new SpeechSynthesisUtterance(spetext1 + spetext2);
@@ -32,11 +32,68 @@ function speak(){
     
 }
 
-link = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/rymJg6zuk/model.json" , start);
+link = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/eM4I2N6xf/model.json" , start);
 
 //PARAMETER3 IS IN THE INITILISZATION SECTION
 function start(){
     console.log("You have passed parameter3");
+}
+
+function handslap(){
+    image = document.getElementById("imu");
+    link.classify(image , chocker);
+}
+
+function chocker(error ,  result){
+    if(error){
+        console.error(error);      
+    }
+    else{
+        console.log(result);        
+
+        document.getElementById("pro1").innerHTML = result[0].label;
+        hatext1 = result[0].label;
+        
+        document.getElementById("pro2").innerHTML = result[1].label;
+        hatext2 = result[1].label;
+
+        speak12();
+        
+        if(result[0].label == "SUPERB"){
+            document.getElementById("pro3").innerHTML = "&#128076;"; 
+        }
+        if(result[0].label == "UP POINTER"){
+            document.getElementById("pro3").innerHTML = "&#128070;"; 
+        }
+        if(result[0].label == "SPLIT FINGER"){
+            document.getElementById("pro3").innerHTML = "&#128406;"; 
+        }
+        if(result[0].label == "SPIDERMAN"){
+            document.getElementById("pro3").innerHTML = "&#129304;"; 
+        }
+        if(result[0].label == "YO"){
+            document.getElementById("pro3").innerHTML = "&#9996;"; 
+        }
+
+
+
+        if(result[1].label == "SUPERB"){
+            document.getElementById("pro4").innerHTML = "&#128076;"; 
+        }
+        if(result[1].label == "UP POINTER"){
+            document.getElementById("pro4").innerHTML = "	&#128070;";  
+        }
+        if(result[1].label == "SPLIT FINGER"){
+            document.getElementById("pro4").innerHTML = "&#128406;"; 
+        }
+        if(result[1].label == "SPIDERMAN"){
+            document.getElementById("pro4").innerHTML = "&#129304;";  
+        }
+        if(result[1].label == "YO"){
+            document.getElementById("pro4").innerHTML = "&#9996;";   
+        }
+
+    }
 }
 
 
